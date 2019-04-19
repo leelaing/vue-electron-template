8aec743eed3818320103d8b78f983c913fadaa98
leelaing <leelaing@liveloveyourlifenow.com>
2019-04-19 06:26:35 -0400

    Updated Firebase.js Added .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,27 @@
 import { initializeApp } from 'firebase'
 
 const app = initializeApp({
-  apiKey: "AIzaSyDZXaU_ng1zOu_HDyCJFc2CvriTzfSdv0I",
-  authDomain: "big2tiny-38f5a.firebaseapp.com",
-  databaseURL: "https://big2tiny-38f5a.firebaseio.com",
-  projectId: "big2tiny-38f5a",
-  storageBucket: "big2tiny-38f5a.appspot.com",
-  messagingSenderId: "709284831928"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID
 })
 
-export const db = app.database()
-export const namesRef = db.ref('names')
-export const postsRef = db.ref('posts')
-export const choresRef = db.ref('chores')
-export const songsRef = db.ref('songs')
-export const savedSongsRef = db.ref('savedSongs')
+app.firestore().enablePersistence()
+  .catch(function (err) {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  })
+
+export const auth = app.auth()
+export const rtdb = app.database()
+export const db = app.firestore()
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,64 @@
 <template>
-  <v-container fluid name="home">
-    <h1 class="center">Homepage</h1>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex class="mt-4" v-for="item in items" :key="item.name" xs3>
+        <v-card :to="item.route" :class="`${item.class} card-link curl`" height="150px">
+          <v-card-text class="px-0">
+            <h5 class="cardtext">{{ item.name }}</h5></v-card-text
+            <img width="100%" :src="item.image" alt="" />
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      name: '',
+      items: [
+        { name: 'open', href: '', image: '', route: '/open', class: 'open' },
+        { name: 'open', href: '', image: '', route: '/open', class: 'open' },
+        { name: 'open', href: '', image: '', route: '/open', class: 'open' },
+        { name: 'open', href: '', image: '', route: '/open', class: 'open' },
+        { name: 'open', href: '', image: '', route: '/open', class: 'open' },
+        { name: 'open', href: '', image: '', route: '/open', class: 'open' },
+      ]
+    }
+  },
   components: {
 
   }
 }
 </script>
+
+<style lang="scss">
+html {
+  margin-top: 45px;
+  overflow: auto;
+  max-height: 100vh;
+}
+.v-card {
+  background-color: transparent;
+}
+.cardtext {
+  margin: auto;
+  width: 55%;
+  color: white;
+  font-weight: bold;
+  background-color: rgba(126, 126, 126, 0.5);
+}
+.card-link {
+  background-image: url("");
+  background-repeat: no-repeat;
+  background-attachment: scroll;
+  background-position: center;
+  background-size: 210px 150px;
+  border-radius: 50px;
+}
+.open {
+  background-image: url("");
+}
+</style>
